8734fe96e777528905a19ab32ac179edb4b607b4
Evan Scott <probablyup@gmail.com>
2017-08-19 21:39:06 -0400

    chore: remove cssta, add complex tree example for browser

--- tests/native.js
+++ tests/native.js
@@ -3,7 +3,6 @@ import { View } from 'react-native'; // eslint-disable-line
 import { create as render } from 'react-test-renderer';
 import styled from 'styled-components/native';
 import { resetStyleCache } from 'styled-components/lib/models/InlineStyle';
-import cssta from 'cssta/native';
 
 export default {
   'simple component': {
@@ -11,13 +10,6 @@ export default {
       resetStyleCache();
 
       const Component = styled(View)`
-        color: red;
-      `;
-
-      render(<Component />);
-    },
-    cssta: () => {
-      const Component = cssta(View)`
         color: red;
       `;
 
@@ -31,24 +23,6 @@ export default {
 
       const Component = styled(View)`
         color: ${props => (props.danger ? 'red' : 'black')};
-      `;
-
-      const instance = render(<Component />);
-      instance.update(<Component danger />);
-      instance.update(<Component />);
-      instance.update(<Component danger />);
-      instance.update(<Component />);
-      instance.update(<Component danger />);
-      instance.update(<Component />);
-      instance.update(<Component danger />);
-    },
-    cssta: () => {
-      const Component = cssta(View)`
-        color: black;
-
-        &[@danger] {
-          color: red;
-        }
       `;
 
       const instance = render(<Component />);
@@ -77,18 +51,6 @@ export default {
 
       render(<Component />);
     },
-    cssta: () => {
-      const Component = cssta(View)`
-        color: red;
-        flex: 1;
-        font: bold 12px/14px "Helvetica";
-        margin: 1px 2px;
-        padding: 3px 4px;
-        border: 1px solid black;
-      `;
-
-      render(<Component />);
-    },
   },
 
   'prop shorthands with prop changes': {
@@ -102,29 +64,6 @@ export default {
         margin: 1px 2px;
         padding: 3px 4px;
         border: 1px solid black;
-      `;
-
-      const instance = render(<Component />);
-      instance.update(<Component danger />);
-      instance.update(<Component />);
-      instance.update(<Component danger />);
-      instance.update(<Component />);
-      instance.update(<Component danger />);
-      instance.update(<Component />);
-      instance.update(<Component danger />);
-    },
-    cssta: () => {
-      const Component = cssta(View)`
-        color: black;
-        flex: 1;
-        font: bold 12px/14px "Helvetica";
-        margin: 1px 2px;
-        padding: 3px 4px;
-        border: 1px solid black;
-
-        &[@danger] {
-          color: red;
-        }
       `;
 
       const instance = render(<Component />);
