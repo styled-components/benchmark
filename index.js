182a5f44410dba35ebde75a9d0243c03c3884231
Evan Scott <probablyup@gmail.com>
2017-10-13 19:02:31 -0400

    refactor: cleanup, annotate table headings with version #

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Benchmark = require('benchmark');
-const nativeTests = require('./compiled/native').default;
-const browserTests = require('./compiled/browser').default;
 const AsciiTable = require('ascii-table');
 const progress = require('pace');
+const nativeTests = require('./compiled/native').default;
+const browserTests = require('./compiled/browser').default;
 
-function getTotalTestCount() {
+function getTotalTestCount(suites) {
   let total = 0;
 
-  [browserTests, nativeTests].forEach((suite) => {
+  Object.values(suites).forEach((suite) => {
     Object.keys(suite).forEach((test) => {
       Object.keys(suite[test]).forEach(() => {
         total += 1;
@@ -18,62 +18,73 @@ function getTotalTestCount() {
   return total;
 }
 
-const bar = progress(getTotalTestCount());
+function runSuites(suites) {
+  const bar = progress(getTotalTestCount(suites));
 
-function runSuite(suiteName, tests) {
-  return new Promise((resolve) => {
-    const suite = new Benchmark.Suite(suiteName, {
-      minSamples: 50,
-    });
-
-    const testNames = Object.keys(tests);
-    const projectNamesSet = new Set();
-
-    const getRunName = (testName, projectName) => `${testName}: ${projectName}`;
-
-    testNames.forEach((testName) => {
-      Object.keys(tests[testName]).forEach((projectName) => {
-        projectNamesSet.add(projectName);
-        suite.add(getRunName(testName, projectName), tests[testName][projectName]);
+  function runSuite(suiteName, tests) {
+    return new Promise((resolve) => {
+      const suite = new Benchmark.Suite(suiteName, {
+        minSamples: 50,
       });
+
+      const testNames = Object.keys(tests);
+      const projectNamesSet = new Set();
+
+      const getRunName = (testName, projectName) => `${testName}: ${projectName}`;
+
+      testNames.forEach((testName) => {
+        Object.keys(tests[testName]).forEach((projectName) => {
+          projectNamesSet.add(projectName);
+          suite.add(getRunName(testName, projectName), tests[testName][projectName]);
+        });
+      });
+
+      const projectNames = Array.from(projectNamesSet);
+
+      /**
+      * something in the dep tree is fubaring the string "styled-components"
+      * when used as a suite key
+      */
+      const projectNamesWithVersions = projectNames.map(
+        // eslint-disable-next-line
+        lib => `${lib} (v${require(`${lib === 'styled' ? 'styled-components' : lib}/package.json`).version})`
+      );
+
+      const table = new AsciiTable(suiteName);
+
+      table.setHeading('', ...projectNamesWithVersions);
+
+      suite.on('cycle', () => bar.op());
+
+      suite.on('error', err => console.error(err.target.error) && suite.abort());
+
+      suite.on('complete', () => {
+        const suiteResults = Array.from(suite);
+        const rows = testNames.map(testName => (
+          [testName].concat(projectNames.map((projectName) => {
+            const runName = getRunName(testName, projectName);
+            const run = suiteResults.find(run => run.name === runName); // eslint-disable-line
+            return run
+              ? `${(run.stats.mean * 1000).toFixed(3)}±${(run.stats.deviation * 1000).toFixed(3)}ms`
+              : '-';
+          }))
+        ));
+
+        rows.forEach(row => table.addRow(row));
+
+        resolve(table.toString());
+      });
+
+      suite.run();
     });
+  }
 
-    const projectNames = Array.from(projectNamesSet);
-    const table = new AsciiTable(suiteName);
+  const results = Object.keys(suites).map(suiteName => runSuite(suiteName, suites[suiteName]));
 
-    table.setHeading('', ...projectNames);
-
-    suite.on('cycle', () => bar.op());
-
-    suite.on('error', err => console.error(err.target.error) && suite.abort());
-
-    suite.on('complete', () => {
-      const suiteResults = Array.from(suite);
-      const rows = testNames.map(testName => (
-        [testName].concat(projectNames.map((projectName) => {
-          const runName = getRunName(testName, projectName);
-          const run = suiteResults.find(run => run.name === runName); // eslint-disable-line
-          return run
-            ? `${(run.stats.mean * 1000).toFixed(3)}±${(run.stats.deviation * 1000).toFixed(3)}ms`
-            : '-';
-        }))
-      ));
-
-      rows.forEach(row => table.addRow(row));
-
-      resolve(table.toString());
-    });
-
-    suite.run();
-  });
+  results.forEach(async result => console.log(await result));
 }
 
-Promise.all([
-  runSuite('browser', browserTests),
-  runSuite('native', nativeTests),
-]).then((tables) => {
-  tables.forEach((table) => {
-    console.log(table);
-    console.log('\n');
-  });
+runSuites({
+  browser: browserTests,
+  native: nativeTests,
 });
